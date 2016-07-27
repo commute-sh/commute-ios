7f865c802aa974831ddd83d301434735df288c4b
Alexis Kinsella <alexis.kinsella@gmail.com>
2016-07-28 00:55:18 +0200

    using custom annotation

--- components/MapTabScene.js
+++ components/MapTabScene.js
@@ -12,21 +12,11 @@ import Map from './Map';
 import StationToast from './StationToast';
 import * as StationService from '../services/StationService';
 
-const pinGreen = require('../images/location-pin-green@2x.png');
-const pinYellow = require('../images/location-pin-yellow@2x.png');
-const pinOrange = require('../images/location-pin-orange@2x.png');
-const pinRed = require('../images/location-pin-red@2x.png');
-const pinBlack = require('../images/location-pin-black@2x.png');
-
-const smallPinGreen = require('../images/location-pin-green-small@2x.png');
-const smallPinYellow = require('../images/location-pin-yellow-small@2x.png');
-const smallPinOrange = require('../images/location-pin-orange-small@2x.png');
-const smallPinRed = require('../images/location-pin-red-small@2x.png');
-const smallPinBlack = require('../images/location-pin-black-small@2x.png');
-
 import reactMixin from 'react-mixin';
 
 import Subscribable from 'Subscribable';
+
+import StationAnnotation from './StationAnnotation';
 
 class MapTabScene extends Component {
 
@@ -53,60 +43,8 @@ class MapTabScene extends Component {
         this.stationToasterDisappear = this.stationToasterDisappear.bind(this);
         this.onChange = this.onChange.bind(this);
         this.mapStationToAnnotation = this.mapStationToAnnotation.bind(this);
+        this.distanceFromPosition = this.distanceFromPosition.bind(this);
         this.useSmallPins = this.useSmallPins.bind(this);
-
-        this.pinImages = {
-            'red': {
-                'small': {
-                    'opaque': <Image source={smallPinRed}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={smallPinRed} style={{ opacity: 0.25, zIndex: 3 }} />
-                },
-                'big': {
-                    'opaque': <Image source={pinRed}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={pinRed} style={{ opacity: 0.25, zIndex: 3 }} />
-                }
-            },
-            'yellow': {
-                'small': {
-                    'opaque': <Image source={smallPinYellow}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={smallPinYellow} style={{ opacity: 0.25 }} />
-                },
-                'big': {
-                    'opaque': <Image source={pinYellow}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={pinYellow} style={{ opacity: 0.25, zIndex: 3 }} />
-                }
-            },
-            'green': {
-                'small': {
-                    'opaque': <Image source={smallPinGreen}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={smallPinGreen} style={{ opacity: 0.25 }} />
-                },
-                'big': {
-                    'opaque': <Image source={pinGreen}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={pinGreen} style={{ opacity: 0.25, zIndex: 3 }} />
-                }
-            },
-            'orange': {
-                'small': {
-                    'opaque': <Image source={smallPinOrange}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={smallPinOrange} style={{ opacity: 0.25, zIndex: 3 }} />
-                },
-                'big': {
-                    'opaque': <Image source={pinOrange}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={pinOrange} style={{ opacity: 0.25, zIndex: 3 }} />
-                }
-            },
-            'black': {
-                'small': {
-                    'opaque': <Image source={smallPinBlack}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={smallPinBlack} style={{ opacity: 0.25, zIndex: 3 }} />
-                },
-                'big': {
-                    'opaque': <Image source={pinBlack}  style={{ zIndex: 3 }} />,
-                    'transparent': <Image source={pinBlack} style={{ opacity: 0.25, zIndex: 3 }} />
-                }
-            }
-        };
     }
 
     onChange(event) {
@@ -280,22 +218,6 @@ class MapTabScene extends Component {
             station.geoPosition = new GeoPoint(station.position.lat, station.position.lng);
         }
 
-        let pinColor = 'green';
-
-        if (station.status === 'CLOSED') {
-            pinColor = 'black';
-        } else if (station.available_bike_stands === 0 || station.available_bikes === 0) {
-            pinColor = 'red';
-        } else if (station.available_bike_stands <= 3 || station.available_bikes <= 3) {
-            pinColor = 'orange';
-        } else if (station.available_bike_stands <= 5 || station.available_bikes <= 5) {
-            pinColor = 'yellow';
-        }
-
-        let useSmallPin = this.useSmallPins();
-
-        let distanceFromPosition = this.distanceFromPosition(station);
-
         const stationNumber = String(station.number);
 
         return {
@@ -305,14 +227,13 @@ class MapTabScene extends Component {
             onFocus: this.onStationFocus.bind(this, station),
             onBlur: this.onStationBlur.bind(this, station),
             view:
-                <View>
-                    {this.pinImages[pinColor][useSmallPin ? 'small' : 'big'][distanceFromPosition < 1000 ? 'opaque' : 'transparent']}
-                    {
-                        <Text style={{ zIndex: 4, position: 'absolute', top: 24, left: 28, color: 'white', backgroundColor: 'rgba(0,0,0,0)', fontSize: 12 }}>
-                            {this.state.annotationTextInfo === 'STANDS' ? station.available_bike_stands : station.available_bikes}
-                        </Text>
-                    }
-                </View>
+                <StationAnnotation
+                    station={station}
+                    textInfo={() => this.state.annotationTextInfo}
+                    useSmallPins={this.useSmallPins}
+                    distanceFromPosition={this.distanceFromPosition}
+                    style={{ width: 64, height: 64 }}
+                />
         };
 
     }
